a37358839f78d9a22787057acee1d43066d2490a
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-11-07 01:31:23 +0000

    `webpack` configuration

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: './bundle.js',
+        path: path.resolve(__dirname, 'bin')
+    },
+    devServer: {
+        contentBase: [path.join(__dirname, 'public'), path.join(__dirname, 'bin')], // both src and output dirs
+        compress: true,
+        port: 7777
+    }
+};
