5a20a5a029f2a3f477343a6f68fdd9937ed08854
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-11-08 22:57:07 +0000

    Add build script for production environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+let production = process.env.NODE_ENV === 'production';
+
+let config = {
     entry: ['./src/index.js', './src/other.js'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     devtool: "inline-source-map",
-    mode: "development",
+    mode: 'development',
     devServer: {
         static: './dist'
     },
@@ -18,3 +20,10 @@ module.exports = {
         }),
     ],
 };
+
+if (production) {
+    config.mode = 'production';
+    config.devtool = "inline-source-map";
+}
+
+module.exports = config;
