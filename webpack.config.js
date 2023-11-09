a1db03d1fa7c4a6907d4227e646da0dd848d79c9
Ozgur Sarikamis <ozgur@techpassport.io>
2023-11-09 12:43:48 +0000

    combining image names with hash

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ let config = {
                 exclude: /node_modules/,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'pictures/logos/[name][ext]'
+                    filename: 'pictures/logos/[hash]-[name][ext]'
                 }
             }
         ]
