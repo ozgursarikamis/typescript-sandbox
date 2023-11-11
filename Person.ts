71e75a30c0d54a9948ce0381b51a00cc7584e6c9
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-11-11 02:00:18 +0000

    initial version of a simple class

---
+++ Person.ts
@@ -0,0 +1,12 @@
+export class Person {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log(`Hello, ${this.name}`);
+    }
+}
+
+new Person("Ozzy").greet();
