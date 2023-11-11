f16a2624e7e5a2b6cf02270f6a1e64be80f6019e
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-11-11 02:07:42 +0000

    A simple method decorator

--- Person.ts
+++ Person.ts
@@ -1,13 +1,22 @@
+function loggedMethod(originalMethod: Function, context: any) {
+    function replacementMethod(this: any, ...args: any[]) {
+        console.log("ENTERING THE METHOD");
+        const result = originalMethod.apply(context, args);
+        console.log("EXITING THE METHOD");
+        return result;
+    }
+    return replacementMethod;
+}
+
 export class Person {
     name: string;
     constructor(name: string) {
         this.name = name;
     }
 
+    @loggedMethod
     greet() {
-        console.log("ENTERING THE METHOD");
         console.log(`Hello, ${this.name}`);
-        console.log("EXITING THE METHOD");
         // This pattern is fairly common.
         // It sure would be nice if there was a way we could do this for every method!
     }
