cc9c7c1ebcedac8a9d59a7abdbf777a4523bc88e
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-11-11 03:20:14 +0000

    Generic Class: Constrains

--- generic-classes.ts
+++ generic-classes.ts
@@ -1,4 +1,4 @@
-export class GenericClass<T> {
+export class GenericClass<T extends number | string> {
     private _value: T | undefined;
     set value(value: T) {
         this._value = value;
@@ -11,3 +11,10 @@ export class GenericClass<T> {
 var genericClass = new GenericClass<number>();
 genericClass.value = 123;
 console.log(genericClass.value);
+
+var genericClass2 = new GenericClass<string>();
+genericClass2.value = "hello";
+console.log(genericClass2.value);
+
+// This will throw a compilation error because boolean is not a valid type for GenericClass
+// var genericClass3 = new GenericClass<boolean>();
