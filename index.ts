be3e52dca844e5db5db287706bb687cdfe988552
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-12-19 22:44:39 +0000

    Type Intersections

--- index.ts
+++ index.ts
@@ -3,3 +3,8 @@ import TypesDeepDive from "./types/types-deep-dive";
 const { name, numbers, names } = TypesDeepDive;
 
 console.log("TypesDeepDive: ", name, numbers, names);
+
+const { partner, customer, employee } = TypesDeepDive;
+console.log("TypesDeepDive Partner:", partner);
+console.log("TypesDeepDive Customer:", customer);
+console.log("TypesDeepDive Employee:", employee);
--- types/types-deep-dive.ts
+++ types/types-deep-dive.ts
@@ -3,6 +3,53 @@
 const name: "John" | "Marry" = "John";
 
 const numbers: Array<number> = [1, 2, 3, 4, 5];
-const names: Array<string> = ["John", "Marry", "Jane"];
+const names: Array<string> = ["John", "Marry"];
 
-export default { name, numbers, names };
+interface BusinessPartner {
+    name: string;
+    credit: number;
+};
+
+interface Identity {
+    id: number;
+    name: string;
+};
+
+interface Occupation {
+    occupation: string;
+    experience: number;
+};
+
+interface Contact {
+    email: string;
+    phone: string;
+};
+
+type BusinessPartnerIdentity = BusinessPartner & Identity & Occupation;
+
+type Employee = Identity & Contact;
+type Customer = BusinessPartner & Contact;
+
+const partner: BusinessPartnerIdentity = {
+    id: 1,
+    name: "John",
+    credit: 100,
+    occupation: "Developer",
+    experience: 10
+};
+
+const employee: Employee = {
+    id: 1,
+    name: "John",
+    email: "",
+    phone: ""
+};
+
+const customer: Customer = {
+    name: "John",
+    credit: 100,
+    email: "",
+    phone: ""
+};
+
+export default { name, numbers, names, partner, employee, customer };
