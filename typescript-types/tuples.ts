228a0d4f6b7153f8760b2a847c39a4282b0e80e6
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-12-20 14:28:27 +0000

    Tuples: Destructuring

--- typescript-types/tuples.ts
+++ typescript-types/tuples.ts
@@ -7,3 +7,22 @@ console.log({
     skillType,
     rank
 });
+
+function doSomething(pair: [string, number]) {
+    const a = pair[0];
+    const b = pair[1];
+
+    console.log({ a, b });
+}
+
+doSomething(["hello", 42]);
+// doSomething(["Hello", false]); // Fail
+
+
+function destructureTuple(stringHash: [string, number]) {
+    const [inputString, hash] = stringHash;
+    return { inputString, hash };
+}
+
+const x = destructureTuple(["hello", 42]);
+console.log("x", x);
