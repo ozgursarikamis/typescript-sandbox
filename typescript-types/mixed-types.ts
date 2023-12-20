30e5b781ee062f0c4ddee89fba51a20ac6b84ed4
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-12-20 13:03:20 +0000

    Types: Mixed Types

---
+++ typescript-types/mixed-types.ts
@@ -0,0 +1,10 @@
+const scores = ['Programming', 5, 'Software Design', 4];
+
+// this is equivalent to:
+const scores2: (number | string)[] = ['Programming', 5, 'Software Design', 4];
+
+console.log({
+    scores,
+    scores2
+});
+
